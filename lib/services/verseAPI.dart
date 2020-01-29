89d2c658ef721574ce245ec84c0015ff3f5d88a9
Huy Vo <voxqhuy@gmail.com>
2020-01-29 17:14:47 -0500

    add verseAPI: getVerses() and getVersions()

--- lib/services/verseAPI.dart
+++ lib/services/verseAPI.dart
@@ -1,5 +1,6 @@
 import 'package:http/htpp.dart' as http;
 import 'dart:convert';
+import '../models/verse.dart';
 
 class verseAPI {
 
@@ -18,4 +19,18 @@ class verseAPI {
       throw Exception('Failed to load post');
     }
   }
+
+  Future<VerseResponse> getVerses(String version, String book, int chapter, int verse) async {
+    // make the request
+    final response = await http.get(baseUrl + version + '/' + book + '/' + chapter.toString() + '/' + version.toString());
+
+    if (response.statusCode == 200) {
+      // If server returns an OK response, parse the JSON.
+      return VerseResponse.fromJson(json.decode(response.body));
+    } else {
+      // If that response was not OK, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
+  
 }
