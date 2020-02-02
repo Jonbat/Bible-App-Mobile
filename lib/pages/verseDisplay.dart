4147e8946662a7725c4cccee8fefb34525aeb05b
Jonathan Batchelder <jonathanbatchelder@southern.edu>
2020-02-02 14:49:03 -0500

    Next and previous buttons working without special cases

--- lib/pages/verseDisplay.dart
+++ lib/pages/verseDisplay.dart
@@ -2,13 +2,61 @@ import 'package:flutter/material.dart';
 import '../services/verseAPI.dart';
 import '../models/verse.dart';
 
-class VerseDisplay extends StatelessWidget {
+class VerseDisplay extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return VerseDisplayState();
+  }
+}
 
+class VerseDisplayState extends State<VerseDisplay> {
+
+  // [0] : previous verse
+  // [1] : current verse
+  // [2] : next verse
   List<Verse> verses = [
     Verse(book: "Ge", chapter: 1, verse: 1, text: "In the beginning God created the heavens and the earth."),
     Verse(book: "Ge", chapter: 1, verse: 2, text: "And the earth was waste and void; and darkness was upon the face of the deep: and the Spirit of God moved upon the face of the waters."),
     Verse(book: "Ge", chapter: 1, verse: 3, text: "And God said, Let there be light: and there was light.")
   ];
+
+  // change verses to display when the next or previous button is pressed
+  void initializeVerses (VerseResponse response) {
+    verses = [
+      response.prev,
+      response.curr,
+      response.next
+    ];
+  }
+
+  void displayPreviousVerse() {
+    // if there is no previous verse, go to last verse of previous chapter
+    if (verses[0] == null) {
+      throw("ToDo: go to last verse of previous chapter");
+    } else {
+      final previousVerse = VerseAPI().getVerseNum('ASV', verses[0].book, verses[0].chapter, verses[0].verse);
+      previousVerse.then((response) {
+        setState(() {
+          initializeVerses(response);
+        }); 
+      });
+    }
+  }
+
+  void displayNextVerse() {
+    // if there is no next verse, go to first verse of next chapter
+    if (verses[2] == null) {
+      throw("ToDo: go to first verse of next chapter");
+    } else {
+      final nextVerse = VerseAPI().getVerseNum('ASV', verses[2].book, verses[2].chapter, verses[2].verse);
+      nextVerse.then((response) {
+        setState(() {
+          initializeVerses(response);
+        }); 
+      });
+    }
+  }
+
 
   Widget verseTemplate(verse) {
     return Card(
@@ -44,15 +92,19 @@ class VerseDisplay extends StatelessWidget {
   Widget build (BuildContext ctxt) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Genesis 1:2"),
+        // title: new Text("Genesis 1:2"),
+        title: new Text(
+          verses[1].book + ' ' + verses[1].chapter.toString() + ':' + verses[1].verse.toString()
+        ),
         centerTitle: true,
         backgroundColor: Colors.deepPurple,
       ),
       body: Column(
+        
         children: <Widget>[
           Column(
             children: verses.map((verse) => verseTemplate(verse)).toList(),
-          ),
+          ),   
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
@@ -62,7 +114,7 @@ class VerseDisplay extends StatelessWidget {
                 child: RaisedButton(
                   child: Text('Previous'),
                   onPressed: () {
-                  // todo: call the previous verse and reload the page
+                    displayPreviousVerse();     
                   },
                 ),
               ),
@@ -73,7 +125,7 @@ class VerseDisplay extends StatelessWidget {
                 child: RaisedButton(
                   child: Text('Next'),
                   onPressed: () {
-                  // todo: call the next verse and reload the page
+                    displayNextVerse();
                   },
                 ),
               ),
@@ -81,6 +133,7 @@ class VerseDisplay extends StatelessWidget {
           )
         ],
       ),
+      
       
     );
   }
