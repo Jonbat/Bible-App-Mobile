af85d0e8d21358dea3befcf35a004da1ffe4ac3a
Vo Huy <voxqhuy@gmail.com>
2020-02-03 12:23:38 -0500

    Main view: constraint views to top of screen, add paddings add space between button and dropdown buttons, blur the warning text

--- lib/main.dart
+++ lib/main.dart
@@ -131,9 +131,9 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Verse Selection'),
         centerTitle: true,
       ),
-      body: Center(
+      body: Container(
+        padding: const EdgeInsets.all(12.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
           // Version Row
           //Text("Please Reselect Version On intital Load"),
@@ -148,42 +148,45 @@ class _MyHomePageState extends State<MyHomePage> {
 
             ),
           ]),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[          
-            FutureBuilder<List<String>>(
-              future: widget.versions, // a previously-obtained Future<String> or null
-              builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
-                var versiondropdown;
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[          
+              FutureBuilder<List<String>>(
+                future: widget.versions, // a previously-obtained Future<String> or null
+                builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+                  var versiondropdown;
 
-                if (snapshot.hasData) {
-                  versiondropdown = DropdownButton<String>(
-                  value: widget.versionselect,
-                  items: snapshot.data.map((String value) {
-                    return new DropdownMenuItem<String>(
-                      value: value,
-                      child: new Text(value,
-                        textAlign: TextAlign.center,
-                      
-                      ),
+                  if (snapshot.hasData) {
+                    versiondropdown = DropdownButton<String>(
+                    value: widget.versionselect,
+                    items: snapshot.data.map((String value) {
+                      return new DropdownMenuItem<String>(
+                        value: value,
+                        child: new Text(value,
+                          textAlign: TextAlign.center,
+                        
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+
+                        print("User selected ${value}");
+                        versionChanged(value);
+
+                      },
                     );
-                  }).toList(),
-                  onChanged: (value) {
-
-                      print("User selected ${value}");
-                      versionChanged(value);
-
-                    },
-                  );
+                    
+                  } else if (snapshot.hasError) {
+                    versiondropdown = Text("Error");
+                  } else {
+                    versiondropdown = Text("Populating");
+                  }
                   
-                } else if (snapshot.hasError) {
-                  versiondropdown = Text("Error");
-                } else {
-                  versiondropdown = Text("Populating");
-                }
-                
-                return versiondropdown;
-              },
-            ), 
-          ]),
+                  return versiondropdown;
+                },
+              ), 
+            ]),
+          ),
           // Book Row
           Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
             Text("Book: ",
@@ -197,49 +200,52 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ]),
 
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[          
-            FutureBuilder<List<String>>(
-              future: widget.books, // a previously-obtained Future<String> or null
-              builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
-                var bookdropdown;
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[          
+              FutureBuilder<List<String>>(
+                future: widget.books, // a previously-obtained Future<String> or null
+                builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+                  var bookdropdown;
 
-                if (snapshot.hasData) {
-                  bookdropdown = DropdownButton<String>(
-                  value: widget.bookselect,
-                  items: snapshot.data.map((String value) {
-                    return new DropdownMenuItem<String>(
+                  if (snapshot.hasData) {
+                    bookdropdown = DropdownButton<String>(
+                    value: widget.bookselect,
+                    items: snapshot.data.map((String value) {
+                      return new DropdownMenuItem<String>(
 
-                      value: value,
-                      child: new Text(value),
-                      
+                        value: value,
+                        child: new Text(value),
+                        
 /* Centers the text but is not completely centered on page
-                      value: value,
-                      child: Container(child: 
-                      Text(value),
-                      width: 160,
-                      alignment: Alignment.center,
-                      ),
+                        value: value,
+                        child: Container(child: 
+                        Text(value),
+                        width: 160,
+                        alignment: Alignment.center,
+                        ),
 */
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+
+                        print("User selected ${value}");
+                        bookChanged(value);
+
+                      },
                     );
-                  }).toList(),
-                  onChanged: (value) {
-
-                      print("User selected ${value}");
-                      bookChanged(value);
-
-                    },
-                  );
+                    
+                  } else if (snapshot.hasError) {
+                    bookdropdown = Text("Error");
+                  } else {
+                    bookdropdown = Text("Populating");
+                  }
                   
-                } else if (snapshot.hasError) {
-                  bookdropdown = Text("Error");
-                } else {
-                  bookdropdown = Text("Populating");
-                }
-                
-                return bookdropdown;
-              },
-            ), 
-          ]),
+                  return bookdropdown;
+                },
+              ), 
+            ]),
+          ),
           // Chapter Row
           Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
             Text("Chapter: ",
@@ -252,39 +258,42 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ]),
 
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-            FutureBuilder<List<int>>(
-              future: widget.chapters, // a previously-obtained Future<String> or null
-              builder: (BuildContext context, AsyncSnapshot<List<int>> snapshot) {
-                var dropdown;
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              FutureBuilder<List<int>>(
+                future: widget.chapters, // a previously-obtained Future<String> or null
+                builder: (BuildContext context, AsyncSnapshot<List<int>> snapshot) {
+                  var dropdown;
 
-                if (snapshot.hasData) {
-                  dropdown = DropdownButton<String>(
-                  value: widget.chapterselect.toString(),
-                  items: snapshot.data.map((int value) {
-                    return new DropdownMenuItem<String>(
-                      value: value.toString(),
-                      child: new Text(value.toString()),
+                  if (snapshot.hasData) {
+                    dropdown = DropdownButton<String>(
+                    value: widget.chapterselect.toString(),
+                    items: snapshot.data.map((int value) {
+                      return new DropdownMenuItem<String>(
+                        value: value.toString(),
+                        child: new Text(value.toString()),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+
+                        print("User selected ${value}");
+                        chapterChanged(value);
+
+                      },
                     );
-                  }).toList(),
-                  onChanged: (value) {
-
-                      print("User selected ${value}");
-                      chapterChanged(value);
-
-                    },
-                  );
+                    
+                  } else if (snapshot.hasError) {
+                    dropdown = Text("Error");
+                  } else {
+                    dropdown = Text("Populating");
+                  }
                   
-                } else if (snapshot.hasError) {
-                  dropdown = Text("Error");
-                } else {
-                  dropdown = Text("Populating");
-                }
-                
-                return dropdown;
-              },
-            ), 
-          ]),
+                  return dropdown;
+                },
+              ), 
+            ]),
+          ),
           // Verse Row
           Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
             Text("Verse: ",
@@ -297,39 +306,42 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ]),
 
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-            FutureBuilder<List<int>>(
-              future: widget.verses, // a previously-obtained Future<String> or null
-              builder: (BuildContext context, AsyncSnapshot<List<int>> snapshot) {
-                var tdropdown;
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, right: 8.0, bottom: 20.0, left: 8.0),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              FutureBuilder<List<int>>(
+                future: widget.verses, // a previously-obtained Future<String> or null
+                builder: (BuildContext context, AsyncSnapshot<List<int>> snapshot) {
+                  var tdropdown;
 
-                if (snapshot.hasData) {
-                  tdropdown = DropdownButton<String>(
-                  value: widget.verseselect.toString(),
-                  items: snapshot.data.map((int value) {
-                    return new DropdownMenuItem<String>(
-                      value: value.toString(),
-                      child: new Text(value.toString(), textAlign: TextAlign.center), 
+                  if (snapshot.hasData) {
+                    tdropdown = DropdownButton<String>(
+                    value: widget.verseselect.toString(),
+                    items: snapshot.data.map((int value) {
+                      return new DropdownMenuItem<String>(
+                        value: value.toString(),
+                        child: new Text(value.toString(), textAlign: TextAlign.center), 
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+
+                        print("User selected ${value}");
+                        verseChanged(value);
+
+                      },
                     );
-                  }).toList(),
-                  onChanged: (value) {
-
-                      print("User selected ${value}");
-                      verseChanged(value);
-
-                    },
-                  );
+                    
+                  } else if (snapshot.hasError) {
+                    tdropdown = Text("Error");
+                  } else {
+                    tdropdown = Text("Populating");
+                  }
                   
-                } else if (snapshot.hasError) {
-                  tdropdown = Text("Error");
-                } else {
-                  tdropdown = Text("Populating");
-                }
-                
-                return tdropdown;
-              },
-            ), 
-          ]),
+                  return tdropdown;
+                },
+              ), 
+            ]),
+          ),
 /* Original Verse Selection Button
           RaisedButton(
             child: Text('Open Verse'),
@@ -345,28 +357,28 @@ class _MyHomePageState extends State<MyHomePage> {
           */
 // Used this to match buttons on Verse View
           ButtonTheme(
-                minWidth: 160.0,
-                colorScheme: ColorScheme.dark(),
-                // To change to iOS, replace 'RaisedButton' with ;CupertinoButton;
-                // and import 'package:flutter/cupertino.dart';
-                child: RaisedButton(
-                  child: Text('Find Verse'),
-                  onPressed: () {
+            minWidth: 160.0,
+            colorScheme: ColorScheme.dark(),
+            // To change to iOS, replace 'RaisedButton' with ;CupertinoButton;
+            // and import 'package:flutter/cupertino.dart';
+            child: RaisedButton(
+              child: Text('Find Verse'),
+              onPressed: () {
 
-                    Navigator.push(
-                      context, 
-                      MaterialPageRoute(builder: (context) => VerseDisplay(widget.versionselect, widget.bookselect, widget.chapterselect, widget.verseselect))
-              );
+                Navigator.push(
+                  context, 
+                  MaterialPageRoute(builder: (context) => VerseDisplay(widget.versionselect, widget.bookselect, widget.chapterselect, widget.verseselect))
+          );
 
-                  },
-                ),
-              ),
+              },
+            ),
+          ),
               
           Text("Please reselect version on intital load",
             textAlign: TextAlign.center,
             style: TextStyle(
               fontSize: 15.0,
-              color: Colors.deepOrangeAccent[700]
+              color: Colors.deepOrangeAccent
               )
           ),
 
