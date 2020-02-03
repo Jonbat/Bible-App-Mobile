3acab29b55b80a3baec7600de3c392b45cc706f4
hondocubansanchez <aj.sanchez111@gmail.com>
2020-02-03 01:14:53 -0500

    Fixed carli's botched commit. The debug wasnt working so I added it back in.'

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:bible_app_mobile/pages/verseDisplay.dart';
 import 'package:flutter/material.dart';
 import 'services/verseAPI.dart';
-// import 'models/verse.dart';
+
 
 Future<void> main() async {
   runApp(MyApp());
@@ -17,8 +17,9 @@ class MyApp extends StatelessWidget {
     
     return MaterialApp(
       title: 'Flutter Bible App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.deepPurple
       ),
       home: MyHomePage(title: 'Flutter Bible App'),
     );
